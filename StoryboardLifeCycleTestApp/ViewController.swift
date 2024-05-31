f393be27181b0798cab2ade9e8485f4027657ce0
wglee0511 <ranazang@naver.com>
2024-05-31 15:08:15 +0900

    feature: add open url button

--- StoryboardLifeCycleTestApp/ViewController.swift
+++ StoryboardLifeCycleTestApp/ViewController.swift
@@ -9,6 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func onPressSetting(_ sender: Any) {
+        guard let url = URL(string: UIApplication.openSettingsURLString) else  {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url
+            )
+        }
+    }
+    
+    @IBAction func onPressOpenUrl(_ sender: Any) {
+        guard let url = URL(string: "https://m.naver.com") else  {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url
+            )
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,7 +45,7 @@ class ViewController: UIViewController {
 //        guard let sceneDelegate = view.window?.windowScene?.delegate as? SceneDelegate else {
 //            return
 //        }
-        
+
         print(appDelegate.sharedAppDelegateData)
         print(appScene.sharedSceneDelegateData)
 //        print(sceneDelegate.sharedSceneDelegateData)
