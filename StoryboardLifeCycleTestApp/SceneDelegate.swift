d695bc4c4ca433cba6f365ac34cac461d9c51714
wglee0511 <ranazang@naver.com>
2024-05-31 14:57:00 +0900

    feature: store

--- StoryboardLifeCycleTestApp/SceneDelegate.swift
+++ StoryboardLifeCycleTestApp/SceneDelegate.swift
@@ -11,7 +11,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    var sharedSceneDelegateData = 0
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
--- StoryboardLifeCycleTestApp/ViewController.swift
+++ StoryboardLifeCycleTestApp/ViewController.swift
@@ -12,6 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        guard let appScene = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else {
+            return
+        }
+        
+//        guard let sceneDelegate = view.window?.windowScene?.delegate as? SceneDelegate else {
+//            return
+//        }
+        
+        print(appDelegate.sharedAppDelegateData)
+        print(appScene.sharedSceneDelegateData)
+//        print(sceneDelegate.sharedSceneDelegateData)
     }
 
 
